5658f11713d64971e24980b1f9b70a692bc86a1c
JoeMWatson <joewatson125@gmail.com>
2020-12-12 13:27:09 +0100

    updatet swimmer

--- gym/envs/mujoco/swimmer.py
+++ gym/envs/mujoco/swimmer.py
@@ -3,17 +3,21 @@ from gym import utils
 from gym.envs.mujoco import mujoco_env
 
 class SwimmerEnv(mujoco_env.MujocoEnv, utils.EzPickle):
+
+    ctrl_cost_coeff = 0.0001
+
     def __init__(self):
         mujoco_env.MujocoEnv.__init__(self, 'swimmer.xml', 4)
         utils.EzPickle.__init__(self)
 
     def _step(self, a):
-        ctrl_cost_coeff = 0.0001
-        xposbefore = self.model.data.qpos[0, 0]
+        # xposbefore = self.model.data.qpos[0, 0]
         self.do_simulation(a, self.frame_skip)
-        xposafter = self.model.data.qpos[0, 0]
-        reward_fwd = (xposafter - xposbefore) / self.dt
-        reward_ctrl = - ctrl_cost_coeff * np.square(a).sum()
+        # xposafter = self.model.data.qpos[0, 0]
+        # reward_fwd = (xposafter - xposbefore) / self.dt
+        # replace weird velocity estimate above with actual velocity (???)
+        reward_fwd = self.model.data.qvel[0, 0]
+        reward_ctrl = - sefl.ctrl_cost_coeff * np.square(a).sum()
         reward = reward_fwd + reward_ctrl
         ob = self._get_obs()
         return ob, reward, False, dict(reward_fwd=reward_fwd, reward_ctrl=reward_ctrl)
